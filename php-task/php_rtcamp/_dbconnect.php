6f2d43fc2aa0590d074aeac7fe18d9a294e3d297
Krishnanvijay173 <76086145+Krishnanvijay173@users.noreply.github.com>
2021-12-27 11:54:49 +0530

    Update _dbconnect.php

--- php-task/php_rtcamp/_dbconnect.php
+++ php-task/php_rtcamp/_dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
 session_start();
-$conn=new mysqli('localhost','root','','krishnan');
+$conn=new mysqli('https://rtcampkrishnanvijay173.herokuapp.com/','root','','krishnan');
 if(mysqli_connect_error())
 {
     die ('connection not found'.mysqli_connect_error());
